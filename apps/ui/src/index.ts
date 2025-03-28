ca528b468008865d5cb9e718dd69ec5c0e369251
satesh charan <sateshcharan@sateshs-MacBook-Air.local>
2025-03-28 09:00:28 +0530

    initial pages added

--- apps/ui/src/index.ts
+++ apps/ui/src/index.ts
@@ -1,3 +1,9 @@
 export * from './button'
 export * from './card'
 export * from './input'
+export * from './label'
+
+//components
+export * from './components/pricing-card'
+export * from './components/login-form'
+export * from './components/signup-form'
