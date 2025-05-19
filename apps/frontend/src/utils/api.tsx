ef775cc4a8e28b2292f9f19a99203517fdbf8b69
satesh charan <sateshcharans@gmail.com>
2025-05-19 23:10:52 +0530

    refactor

--- apps/frontend/src/utils/api.tsx
+++ apps/frontend/src/utils/api.tsx
@@ -6,6 +6,8 @@ const api = axios.create({
   baseURL: isDev ? 'http://localhost:3000' : import.meta.env.VITE_API_URL,
 });
 
+console.log(import.meta.env.VITE_API_URL);
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -14,4 +16,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api ;
+export default api;
--- libs/prisma/src/lib/prismaTypes.ts
+++ libs/prisma/src/lib/prismaTypes.ts
@@ -1,7 +1,3 @@
-// import { UserType, FileType, File, Org } from '@prisma/client';
-
 import { UserType, FileType } from '@prisma/client';
-
-// export type { UserType, FileType, File, Org };
 
 export { UserType, FileType };
