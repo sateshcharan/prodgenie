4d394e2eb1c953e90ae774b0912d59c7ff6b714b
satesh charan <sateshcharans@gmail.com>
2025-07-27 18:47:48 +0530

    redis update

--- libs/redis/src/lib/redis.ts
+++ libs/redis/src/lib/redis.ts
@@ -6,8 +6,17 @@
 //   maxRetriesPerRequest: null,
 // });
 
-export const connection = {
-  host: process.env.REDIS_URL || 'localhost',
-  port: 6379,
-  maxRetriesPerRequest: null,
-};
+// export const connection = {
+//   host: process.env.REDIS_URL || 'localhost',
+//   port: 6379,
+//   maxRetriesPerRequest: null,
+// };
+
+import IORedis from 'ioredis';
+
+export const connection = new IORedis(
+  process.env.REDIS_URL || 'redis://localhost:6379',
+  {
+    maxRetriesPerRequest: null,
+  }
+);
